bdae1de226ed210b95b437dcf53ecf71a299b4cb
Ashish Phogat <118557570+Ashish-Phogat@users.noreply.github.com>
2022-12-03 14:59:24 +0530

    Add files via upload

---
+++ while loop  in python.py
@@ -0,0 +1,37 @@
+i = 0
+while i<10:
+    print("Yes" + str(i))
+    i = i+1
+# while is done only for true condition.
+
+i = 0
+while i < 50:
+    print(str(i))
+    i = i+1
+
+i = 2
+while i < 50:
+    print(str(i))
+    i = i+2
+
+i = 7
+while i < 10:
+    print(str(i))
+    i = i+1
+i = 0
+while i<5:
+    print("ashish")
+    i = i+1
+
+i = 0
+while i<99:
+    print("life")
+    i = i +1
+
+# Content While loop
+
+fruits = ["Banana", "watermelon", "guvava","Mango"]
+i = 0
+while i<len(fruits):
+    print(fruits[i])
+    i = i+1
